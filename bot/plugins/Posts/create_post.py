e8a9a8aa4049c53b4bfe1bd964affee71fb09344
Pranav <npv12@iitbbs.ac.in>
2022-04-03 19:50:35 +0530

    feat: Make the generated post truly dynamic
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- bot/plugins/Posts/create_post.py
+++ bot/plugins/Posts/create_post.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from typing import List
 from pyrogram import filters, Client
+from bot.database.maintainer_details import maintainer_details
 from bot.utils.logging import logger
 from bot.utils.parser import find_device, parse_post_links
 
@@ -16,27 +18,45 @@ async def create_post(client, message):
         return
 
     parsed_links: dict = parse_post_links(message.command[1:])
+
+    download_link_str: str = "Download:\n"
+
+    possible_links = ["rom", "fastboot", "incremental", "boot"]
+
+    for link_type in possible_links:
+
+        if len(parsed_links[link_type]) > 0:
+            download_link_str += f"*    [{link_type.capitalize()}]({parsed_links[link_type][0]}) "
+            for i in range(1, len(parsed_links[link_type])):
+                download_link_str += f"[Mirror {i}]({parsed_links[link_type][i]}) "
+
+            download_link_str += "\n"
+
     date = datetime.today().strftime('%Y-%m-%d')
     device = find_device(message.command[1])
 
+    maintainers: List[dict] = maintainer_details.get_maintainers(device)
+    maintainer_str: str = ""
+    for maintainer in maintainers:
+        maintainer_str += f"[{maintainer['name']}] (tg://user?id={maintainer['user_id']}) "
+
+    device_support_group = maintainer_details.get_device_support_group(device)
+
     caption = f"""
     KOSP 12.1 | OFFICIAL
+    
+    Maintainers: {maintainer_str}
 
-    Maintainers: @darknanobot
     Device: {device}
     Date: {date}
 
-    Download: 
-    *   [Rom]({parsed_links["rom"][0]})
-    *   [Fastboot]({parsed_links["fastboot"][0]})
-    *   [Incremental]({parsed_links["incremental"][0]})
-    *   [Boot]({parsed_links["boot"][0]})
+    {download_link_str}
 
     [Changelog](https://raw.githubusercontent.com/AOSP-Krypton/ota/A12/{device}/changelog_{date})
 
     Support group
     *   [Official](https://t.me/kryptonaosp)
-    *   [Device](https://t.me/kospOP7P)
+    *   [Device]({device_support_group})
     """
 
     await client.send_photo(chat_id=message.chat.id,
