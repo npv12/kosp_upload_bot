58ce8f49fe1b10468015c7d952cf4906f7beb81a
Pranav <npv12@iitbbs.ac.in>
2022-04-03 17:27:14 +0530

    Rearrange files a bit
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- bot/utils/parser.py
+++ bot/utils/parser.py
@@ -5,15 +5,6 @@ from typing import List
 
 from bot.utils.logging import logger
 
-
-def parse_kosp(file_name: str) -> str:
-    logger.info(f"Recieved request to parse a file name as {file_name}")
-    parts: List[str] = file_name.split("-")[1:]
-    android_version = "A" + str(10 + int(float(parts[0])))
-    device_name = parts[1]
-    logger.info(f"It was parsed as {android_version}/{device_name}")
-    return android_version + "/" + device_name
-
 def find_device(file_name: str) -> str:
     logger.info(f"Recieved request to find a device for {file_name}")
 
