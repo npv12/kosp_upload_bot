c02784dd76836a4d113de10667d461a4f708078e
Pranav <npv12@iitbbs.ac.in>
2022-04-03 14:40:05 +0530

    Parse boot.img separately
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- bot/utils/parser.py
+++ bot/utils/parser.py
@@ -16,6 +16,12 @@ def parse_kosp(file_name: str) -> str:
 
 def find_device(file_name: str) -> str:
     logger.info(f"Recieved request to find a device for {file_name}")
+
+    if "boot_" in file_name:
+        device_name = file_name.split("_")[1]
+        logger.info(f"It was parsed as {device_name}")
+        return device_name
+
     parts: List[str] = file_name.split("-")[1:]
     device_name = parts[1]
     logger.info(f"It was parsed as {device_name}")
