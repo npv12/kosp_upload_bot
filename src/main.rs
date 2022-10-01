35948c83542911a99c370669acd51cd4d4431a2a
Pranav <npv12@iitbbs.ac.in>
2022-10-01 16:02:49 +0530

    initial commit
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+//! Example to echo user text messages. Updates are handled concurrently.
+//!
+//! The `TG_ID` and `TG_HASH` environment variables must be set (learn how to do it for
+//! [Windows](https://ss64.com/nt/set.html) or [Linux](https://ss64.com/bash/export.html))
+//! to Telegram's API ID and API hash respectively.
+//!
+//! Then, run it as:
+//!
+//! ```sh
+//! cargo run --example echo -- BOT_TOKEN
+//! ```
+
+use grammers_client::{Client, Config, InitParams, Update};
+use grammers_session::Session;
+use log;
+use simple_logger::SimpleLogger;
+use std::env;
+use tokio::{runtime, task};
+
+type Result = std::result::Result<(), Box<dyn std::error::Error>>;
+
+const SESSION_FILE: &str = "echo.session";
+
+async fn handle_update(client: Client, update: Update) -> Result {
+    match update {
+        Update::NewMessage(message) if !message.outgoing() => {
+            let chat = message.chat();
+            println!("Responding to {}", chat.name());
+            client.send_message(&chat, message.text()).await?;
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
+
+async fn async_main() -> Result {
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Debug)
+        .init()
+        .unwrap();
+
+    let api_id = env!("TG_ID").parse().expect("TG_ID invalid");
+    let api_hash = env!("TG_HASH").to_string();
+    let token = env::args().skip(1).next().expect("token missing");
+
+    println!("Connecting to Telegram...");
+    let mut client = Client::connect(Config {
+        session: Session::load_file_or_create(SESSION_FILE)?,
+        api_id,
+        api_hash: api_hash.clone(),
+        params: InitParams {
+            // Fetch the updates we missed while we were offline
+            catch_up: true,
+            ..Default::default()
+        },
+    })
+    .await?;
+    println!("Connected!");
+
+    if !client.is_authorized().await? {
+        println!("Signing in...");
+        client.bot_sign_in(&token, api_id, &api_hash).await?;
+        client.session().save_to_file(SESSION_FILE)?;
+        println!("Signed in!");
+    }
+
+    println!("Waiting for messages...");
+
+    // This code uses `select!` on Ctrl+C to gracefully stop the client and have a chance to
+    // save the session. You could have fancier logic to save the session if you wanted to
+    // (or even save it on every update). Or you could also ignore Ctrl+C and just use
+    // `while let Some(updates) =  client.next_updates().await?`.
+    while let Some(update) = tokio::select! {
+        _ = tokio::signal::ctrl_c() => Ok(None),
+        result = client.next_update() => result,
+    }? {
+        let handle = client.clone();
+        task::spawn(async move {
+            match handle_update(handle, update).await {
+                Ok(_) => {}
+                Err(e) => eprintln!("Error handling updates!: {}", e),
+            }
+        });
+    }
+
+    println!("Saving session file and exiting...");
+    client.session().save_to_file(SESSION_FILE)?;
+    Ok(())
+}
+
+fn main() -> Result {
+    runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async_main())
+}
