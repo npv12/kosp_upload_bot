2e795d546a05c321bd352ffa4ba9305cf6583795
Pranav <npv12@iitbbs.ac.in>
2022-10-02 18:52:15 +0530

    feat: update the devices if user already is a maintainer

--- src/database/add_maintainer.rs
+++ src/database/add_maintainer.rs
@@ -5,17 +5,30 @@ use bson::doc;
 use super::Db;
 
 impl Db {
-    pub async fn add_maintainer(&self, maintainer: String, user_id: i64, device: String) -> Result<(), Box<dyn Error>> {
+    pub async fn add_maintainer(
+        &self,
+        maintainer: String,
+        user_id: i64,
+        device: String,
+    ) -> Result<(), Box<dyn Error>> {
         let collection = self.db.collection("maintainer");
-        let doc = doc! {
-            "name": maintainer,
-            "user_id": user_id,
-            "is_maintainer": true,
-            "is_admin": false,
-            "devices": [device],
-            "support_group": ""
-        };
-        collection.insert_one(doc, None).await?;
+        let filter = doc! { "user_id": user_id };
+        let found_col = collection.find_one(filter.clone(), None).await?;
+
+        if found_col != None {
+            let update = doc! { "$push": { "devices": device } };
+            collection.update_one(filter.clone(), update, None).await?;
+        } else {
+            let doc = doc! {
+                "name": maintainer,
+                "user_id": user_id,
+                "is_maintainer": true,
+                "is_admin": false,
+                "devices": [device],
+                "support_group": ""
+            };
+            collection.insert_one(doc, None).await?;
+        }
         Ok(())
     }
 }
